2d696d5483a3b8f6db9cfce72ae1bae42ea95300
JohnNarofsky <john.narofsky@gmail.com>
2023-03-29 15:06:52 -0500

    mostly good pull from demo site

---
+++ src/App.js
@@ -0,0 +1,508 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div className="container">
+<div className="parallax">
+    
+     <div className="parallax__layer parallax__layer__0">
+        <img src="./images/dei-logo-x.png" width="3000" hspace="0" vspace="0" alt="Choose Your D E I Adventure" />
+    </div>
+    <div className="parallax__layer parallax__layer__3">
+        <img src="./images/dei_site_layer_mtn_y.png?raw=true" alt="Let the journey begin" />
+    </div>
+    
+     <div className="parallax__layer parallax__layer__6">
+        <img src="./images/dei_site_layer_trees_x.png" alt="Into the Woods" />
+    </div>
+
+    
+    <div className="parallax__cover">
+
+        
+    <div className="cover-content ctop">
+        <div className="section quests">
+            <h1 className="section-top">Welcome Adventurers!</h1>
+            <p className="section-top-content">This guide is for all Game Masters <strong>or GM's</strong> out there looking to start a Choose Your DEI Adventure game. Following these starting quests will help you along your journey. Good luck!</p>
+        </div>
+        
+        <div className="section quests">
+        
+            <h2>First Quest: Assemble the Adventure </h2>
+            <p>
+                Confirm that your company has formal, actionable DEI Goals & Objectives that can be aligned with the actions in the game. Without clear Goals and Objectives, the actions will not be as impactful, and you'll do a lot of work for minimal results. 
+            
+                Find at least one executive sponsor. This person will be the one to make sure that the efforts within the game are prioritized and funded (where needed). 
+    
+                Assemble your Guild Leaders to manage and support the players if they need help with any of the actions in the game. In this guide, we're using six guilds&mdash;<strong>Warrior, Scribe, Cultivator, Wizard, Artisan, and Storyteller</strong>. But feel free to custimize guilds as you like. 
+            
+                Next, gather your employees that are ready to take on a grass-roots initiative to work on DEI. These are the folks that will help the guild leaders design quests and play the game. 
+            
+                Once these are in place, you're ready for your second quest! 
+            </p>
+        </div>
+        <div className="section quests">
+                            
+            <h2>Second Quest: Draw the Map </h2>
+            <p>
+                Align your company Goals & Objectives to actions in the game. Take a look at the actions for each Guild and create Quests for you and your colleagues to embark on to help meet those goals. An example:  
+            </p>
+                
+
+            <div className="action-table-grid">
+            
+                
+                <div>
+                
+                    
+                    <div className="action-table-header"><h3>Company DEI Goal: <span className="quest-detail">Diversity in Hiring/Recruitment</span></h3></div>
+                    <div className="action-table-header"><h3>Quest: <span className="quest-detail">Recruitment Quest</span></h3></div>
+                    <div className="action-table-header"><h3>Required Guilds: <span className="quest-detail">Scribe & Cultivator</span></h3></div>
+                    <div className="action-table-header"><h3>Quest Actions</h3></div>
+                            
+                        
+                    <div className="action-table-container">     
+                    
+                        <table className="action-table quest-examples">
+                            <tr>
+                                <td className="action-table-td left-col">Review Job Descriptions for open roles to remove bias</td>
+                                <td className="action-table-td right-col">75 xp</td>
+                                <td className="action-table-td right-col">Scribe</td>
+                            </tr>
+                            <tr>
+                                <td className="action-table-td left-col">Participate in a Structured Interview</td>
+                                <td className="action-table-td right-col">100 xp</td>
+                                <td className="action-table-td right-col">Cultivator</td>
+                            </tr>
+                            <tr>
+                                <td className="action-table-td left-col bttm-row">Conduct interviews at a diversity-related recruitment event</td>
+                                <td className="action-table-td right-col bttm-row">175 xp</td>
+                                <td className="action-table-td right-col bttm-row">Cultivator</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+            <p>
+                Partner with your grassroots groups in your company, such as Employee Resource Groups. What quests would they like to organize for folks to participate in? 
+            
+                Share the quests you've created with your Executive Sponsor(s). Get their feedback and buy-in before you launch. 
+            
+                Decide how you want to track points until the tracker is finished. Some teams prefer Excel, while others prefer Microsoft Forms for folks to enter the actions they've completed and accumulate points.
+            
+                Now the real fun begins! 
+            </p>
+        </div>  
+          <div className="section quests">      
+                <h2>Third Quest: Gather Your Adventure Parties and Play! </h2>
+                
+        <p>
+                You know what's best for your organization when it comes to rolling out large scale initiatives, so do what works for you. Town Halls, Emails, Intranet Links, Word of Mouth - it's all likely going to be necessary to get folks excited about the game! 
+                
+                As the adventure unfolds, share your stories (and get Storyteller points!) on social media with the hashtag #DEIAdventureGame so we can all be inspired! 
+        </p>
+        
+        </div>
+        <div className="section quests">
+            <h2>Want more information?</h2>
+            Contact <a className="linkedin-link" href="https://www.linkedin.com/in/abbydryer/" target="_new">Abby Dryer</a>
+        </div>  
+        </div>
+        
+
+<div className="cover-div">
+        <img src="./images/dei_site_layer_trees_div_lt.png" alt="More Trees" />
+    </div>
+        
+        
+    <div className="cover-content cbttm">
+                <div className="section quests">
+            <h1 className="section-top">Ideas for Guild Actions</h1>
+            <p className="section-top-content">Members of a guild can complete actions to gain experience points <strong>(XP)</strong>. As adventurers continue to accumulate points, they can ‘level up’ and gain other abilities, like being able to define quests or lead Adventure parties. </p>
+        </div>
+        
+
+<div className="action-table-grid">
+  
+  
+  <div>
+    
+      
+<div className="action-table-header"><h2>Warrior Actions</h2></div>
+      
+    
+<div className="action-table-container">     
+
+<table className="action-table">
+    
+  <tr>
+  <td className="action-table-td left-col">Join an Employee Resource Group (ERG)</td>
+  <td className="action-table-td right-col">20 xp</td>
+  </tr>
+    
+  <tr>
+  <td className="action-table-td left-col">Attend an ERG event</td>
+  <td className="action-table-td right-col">30 xp</td>
+  </tr>
+    
+  <tr>
+  <td className="action-table-td left-col">Attend an ERG Committee meeting</td>
+  <td className="action-table-td right-col">50 xp</td>
+  </tr>
+    
+  <tr>
+  <td className="action-table-td left-col">Find ERG partners for groups</td>
+  <td className="action-table-td right-col">120 xp</td>
+  </tr>
+    
+  <tr>
+  <td className="action-table-td left-col">Be a Guild Leader</td>
+  <td className="action-table-td right-col">200 xp</td>
+  </tr>
+    
+      <tr>
+  <td className="action-table-td left-col">Be a DEI Champion</td>
+  <td className="action-table-td right-col">200 xp</td>
+  </tr>
+    
+      <tr>
+  <td className="action-table-td left-col">Be an ERG Officer or Committee Lead</td>
+  <td className="action-table-td right-col">200 xp</td>
+  </tr>
+    
+      <tr>
+  <td className="action-table-td left-col">Run a DEI-related event</td>
+  <td className="action-table-td right-col">200 xp</td>
+  </tr>
+    
+  <tr>
+  <td className="action-table-td left-col bttm-row">Found an ERG</td>
+  <td className="action-table-td right-col bttm-row">300 xp</td>
+  </tr>
+</table>
+     
+</div>
+   
+    
+   
+  </div>    
+  
+  
+  <div>
+    
+
+      <div className="action-table-header"><h2>Scribe Actions</h2></div>
+      
+    
+<div className="action-table-container">     
+
+<table className="action-table">
+    
+  <tr>
+  <td className="action-table-td left-col">Schedule a DEI meeting</td>
+  <td className="action-table-td right-col">10 xp</td>
+  </tr>
+    
+  <tr>
+  <td className="action-table-td left-col">Update Zoom name with pronouns</td>
+  <td className="action-table-td right-col">15 xp</td>
+  </tr>
+    
+  <tr>
+  <td className="action-table-td left-col">Update email signature with pronouns</td>
+  <td className="action-table-td right-col">15 xp</td>
+  </tr>
+    
+      <tr>
+  <td className="action-table-td left-col">Track a set of DEI metrics</td>
+  <td className="action-table-td right-col">50 xp</td>
+  </tr>
+
+  <tr>
+  <td className="action-table-td left-col">Draft a DEI or ERG-related survey</td>
+  <td className="action-table-td right-col">50 xp</td>
+  </tr>
+    
+  <tr>
+  <td className="action-table-td left-col">Review Job Descriptions to help remove bias</td>
+  <td className="action-table-td right-col">75 xp</td>
+  </tr>
+
+  <tr>
+  <td className="action-table-td left-col">Review a presentation draft for Accessibility needs</td>
+  <td className="action-table-td right-col">75 xp</td>
+  </tr>
+    
+  <tr>
+  <td className="action-table-td left-col">Help plan a DEI-related event</td>
+  <td className="action-table-td right-col">100 xp</td>
+  </tr>
+    
+  <tr>
+  <td className="action-table-td left-col">Create a Fundraising Campaign</td>
+  <td className="action-table-td right-col">200 xp</td>
+  </tr>
+    
+  <tr>
+  <td className="action-table-td left-col bttm-row">Submit a DEI presentation for an external conference</td>
+  <td className="action-table-td right-col bttm-row">250 xp</td>
+  </tr>
+</table>
+  
+</div>
+   
+    
+   
+  </div>    
+  
+  
+   <div>
+    
+       
+<div className="action-table-header"><h2>Cultivator Actions</h2></div>
+      
+    
+<div className="action-table-container">     
+
+<table className="action-table">
+ 
+      <tr>
+  <td className="action-table-td left-col">Celebrate an emplyee's DEI efforts</td>
+  <td className="action-table-td right-col">50 xp</td>
+  </tr>
+    
+      <tr>
+  <td className="action-table-td left-col">Recruit a new member for an ERG</td>
+  <td className="action-table-td right-col">50 xp</td>
+  </tr>
+    
+      <tr>
+  <td className="action-table-td left-col">Involve someone new in a DEI initiative</td>
+  <td className="action-table-td right-col">75 xp</td>
+  </tr>
+    
+      <tr>
+  <td className="action-table-td left-col">Arrange a speaker for a DEI panel</td>
+  <td className="action-table-td right-col">100 xp</td>
+  </tr>
+    
+  <tr>
+  <td className="action-table-td left-col">Conduct interviews at a diversity-related recruitment event</td>
+  <td className="action-table-td right-col">150 xp</td>
+  </tr>
+    
+  <tr>
+  <td className="action-table-td left-col">Find Community Organizations for ERGs to work with</td>
+  <td className="action-table-td right-col">175 xp</td>
+  </tr>
+    
+  <tr>
+  <td className="action-table-td left-col">Submit a colleague for an external DEI Award</td>
+  <td className="action-table-td right-col">175 xp</td>
+  </tr>
+    
+  <tr>
+  <td className="action-table-td left-col">Define DEI metrics for your team</td>
+  <td className="action-table-td right-col">200 xp</td>
+  </tr>
+    
+  <tr>
+  <td className="action-table-td left-col bttm-row">Act as a Mentor or Manager for a DEI program</td>
+  <td className="action-table-td right-col bttm-row">350 xp</td>
+  </tr>
+</table>
+       
+</div>
+   
+  </div>    
+  
+  
+  <div>
+    
+
+<div className="action-table-header"><h2>Wizard Actions</h2></div>
+      
+    
+<div className="action-table-container">     
+
+<table className="action-table">
+    
+    <tr>
+  <td className="action-table-td left-col">Use your education stipend to purchase DEI-related materials or DEI Conference tickets</td>
+  <td className="action-table-td right-col">5 xp</td>
+  </tr>
+    
+<tr>
+  <td className="action-table-td left-col">Complete a DEI-focused Learning Course</td>
+  <td className="action-table-td right-col">30 xp</td>
+  </tr>
+    
+      <tr>
+  <td className="action-table-td left-col">Participate in a DEI-focused Book Club</td>
+  <td className="action-table-td right-col">30 xp</td>
+  </tr>
+    
+      <tr>
+  <td className="action-table-td left-col">Attend a DEI session at an external conference</td>
+  <td className="action-table-td right-col">50 xp</td>
+  </tr>
+    
+      <tr>
+  <td className="action-table-td left-col">Read a chapter in a DEI-related book</td>
+  <td className="action-table-td right-col">50 xp</td>
+  </tr>
+    
+      <tr>
+  <td className="action-table-td left-col">Suggest DEI improvements to your DEI Champion</td>
+  <td className="action-table-td right-col">50 xp</td>
+  </tr>
+    
+  <tr>
+  <td className="action-table-td left-col">Attend an Inclusive Leader Workshop in your region</td>
+  <td className="action-table-td right-col">75 xp</td>
+  </tr>
+    
+  <tr>
+  <td className="action-table-td left-col">Produce reading lists/things to watch</td>
+  <td className="action-table-td right-col">200 xp</td>
+  </tr>
+    
+  <tr>
+  <td className="action-table-td left-col">Create DEI content to share internally</td>
+  <td className="action-table-td right-col">200 xp</td>
+  </tr>
+    
+  <tr>
+  <td className="action-table-td left-col bttm-row">Produce a DEI-focused research paper</td>
+  <td className="action-table-td right-col bttm-row">300 xp</td>
+  </tr>
+</table>
+     
+</div>
+   
+ 
+  </div>      
+
+  
+  <div>
+    
+      
+<div className="action-table-header"><h2>Artisan Actions</h2></div>
+      
+    
+<div className="action-table-container">     
+
+<table className="action-table">
+    
+  <tr>
+  <td className="action-table-td left-col">Complete a ticket from a DEI-related Jira Project (2 or less story points)</td>
+  <td className="action-table-td right-col">50 xp</td>
+  </tr>
+    
+  <tr>
+  <td className="action-table-td left-col">Complete a ticket from a DEI-related Jira Project (3-5 story points)</td>
+  <td className="action-table-td right-col">100 xp</td>
+  </tr>
+    
+  <tr>
+  <td className="action-table-td left-col">Complete a ticket from a DEI-related Jira Project (8 or more story points)</td>
+  <td className="action-table-td right-col">200 xp</td>
+  </tr>
+    
+  <tr>
+  <td className="action-table-td left-col bttm-row">Create art for the DEI Adventure Game</td>
+  <td className="action-table-td right-col bttm-row">200 xp</td>
+  </tr>
+</table>
+    
+    
+</div>
+   
+  </div>    
+    
+  
+  <div>
+    
+<div className="action-table-header"><h2>Storyteller Actions</h2></div>
+      
+    
+<div className="action-table-container">     
+
+<table className="action-table">
+    
+        <tr>
+  <td className="action-table-td left-col">Share a DEI post on social media</td>
+  <td className="action-table-td right-col">10 xp</td>
+  </tr>
+    
+      <tr>
+  <td className="action-table-td left-col">Provide information about a cultural holiday to your team</td>
+  <td className="action-table-td right-col">15 xp</td>
+  </tr>
+    
+      <tr>
+  <td className="action-table-td left-col">Promote a DEI Event</td>
+  <td className="action-table-td right-col">20 xp</td>
+  </tr>
+    
+      <tr>
+  <td className="action-table-td left-col">Share DEI information at a meeting</td>
+  <td className="action-table-td right-col">50 xp</td>
+  </tr>
+    
+      <tr>
+  <td className="action-table-td left-col">Promote an ERG event/meeting</td>
+  <td className="action-table-td right-col">50 xp</td>
+  </tr>
+    
+  <tr>
+  <td className="action-table-td left-col">Produce DEI content for social media</td>
+  <td className="action-table-td right-col">175 xp</td>
+  </tr>
+    
+  <tr>
+  <td className="action-table-td left-col">Be part of a DEI panel</td>
+  <td className="action-table-td right-col">200 xp</td>
+  </tr>
+    
+  <tr>
+  <td className="action-table-td left-col">Write a blog post or article about a DEI event experience</td>
+  <td className="action-table-td right-col">250 xp</td>
+  </tr>
+    
+  <tr>
+  <td className="action-table-td left-col">Produce a DEI-focused research paper</td>
+  <td className="action-table-td right-col">300 xp</td>
+  </tr>
+    
+  <tr>
+  <td className="action-table-td left-col bttm-row">Present at a national/international conference</td>
+  <td className="action-table-td right-col bttm-row">350 xp</td>
+  </tr>
+</table>
+     
+</div>
+   
+    
+   
+  </div>     
+    
+    
+</div>
+
+    
+        
+        </div>  
+        
+    </div>
+        </div>
+        
+            
+    </div>
+    </div>
+  );
+}
+
+export default App;
